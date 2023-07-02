04443ba2749c0765674e1e18228e7af13973a62d
Riiuzaky <jonathans.cruzm@gmail.com>
2023-07-02 17:13:58 -0500

    Proyecto parte visual con responsive

--- src/General/Header.js
+++ src/General/Header.js
@@ -15,24 +15,34 @@ export function Header() {
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-      <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
-        <Link to="/" onClick={handleMenuToggle}>
-          Inicio
-        </Link>
-        <Link to="/eventos" onClick={handleMenuToggle}>
-          Eventos
-        </Link>
-        <Link to="/miembros" onClick={handleMenuToggle}>
-          Miembros
-        </Link>
-        <Link to="/anuncios" onClick={handleMenuToggle}>
-          Anuncios
-        </Link>
-      </div>
-      <div className={`menu-toggle ${isMenuOpen ? 'active' : ''}`} onClick={handleMenuToggle}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+      <div className="nav-container"> {/* Agrega un contenedor para la navegación */}
+        <div className={`menu-toggle ${isMenuOpen ? 'active' : ''}`} onClick={handleMenuToggle}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+          <li>
+            <Link to="/" onClick={handleMenuToggle}>
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link to="/eventos" onClick={handleMenuToggle}>
+              Eventos
+            </Link>
+          </li>
+          <li>
+            <Link to="/miembros" onClick={handleMenuToggle}>
+              Miembros
+            </Link>
+          </li>
+          <li>
+            <Link to="/anuncios" onClick={handleMenuToggle}>
+              Anuncios
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
