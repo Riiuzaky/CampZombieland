5e0203892b23e3147f39a3e7a7396a382b12ee60
Riiuzaky <jonathans.cruzm@gmail.com>
2023-07-02 17:03:01 -0500

    Proyecto parte visual con responsive

--- src/General/Header.js
+++ src/General/Header.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import logo from '../Imagenes/Logo.jpeg';
 
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav>
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-      <ul className="nav-links">
-      <Link to='/'> Inicio </Link>
-      <Link to='/eventos'> Eventos </Link>
-      <Link to='/miembros'> Miembros </Link>
-      <Link to='/anuncios'> Anuncios </Link>
-      </ul>
+      <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+        <Link to="/" onClick={handleMenuToggle}>
+          Inicio
+        </Link>
+        <Link to="/eventos" onClick={handleMenuToggle}>
+          Eventos
+        </Link>
+        <Link to="/miembros" onClick={handleMenuToggle}>
+          Miembros
+        </Link>
+        <Link to="/anuncios" onClick={handleMenuToggle}>
+          Anuncios
+        </Link>
+      </div>
+      <div className={`menu-toggle ${isMenuOpen ? 'active' : ''}`} onClick={handleMenuToggle}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
     </nav>
   );
 }
-
-
